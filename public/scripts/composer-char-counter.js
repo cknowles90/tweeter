16ee84b294babadd44213d463205cf8738622b76
Charles Denahy-Knowles <cjknowles90@gmail.com>
2023-10-10 20:41:07 -0700

    Updated nav.css and index.html for layout developments. Also created a new JS script to handle JQuery functions for browser side features. Added a new .css and html code for the new feature of showing posted tweets.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+  console.log("DOM has loaded");
+
+  $('#tweetText').on('input', function ()  {
+    const inputText = $(this).val().length;
+    const charCount = $(this).closest('.tweetForm').find('.counter');
+    console.log($(this).val().length);
+    // console.log(this);
+
+    if (inputText > 140) {
+      charCount.text(140 - inputText).addClass('invalid');
+    } else {
+      charCount.text(140 - inputText).removeClass('invalid');
+    }
+    
+  });
+});
